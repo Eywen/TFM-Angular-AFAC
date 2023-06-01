c71d2c9f3864761bb9ed8abebaeab6e3b33f451f
Kara <blk20100@gmail.com>
2023-06-01 17:25:52 +0200

    Added custumer add component #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CustomerAddComponent
   ],
   imports: [
     BrowserModule,
