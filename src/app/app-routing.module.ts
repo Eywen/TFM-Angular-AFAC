889a5c9d7e1a11b6d82b27ee23f3fc5467767e7b
Kara <blk20100@gmail.com>
2023-06-16 16:10:14 +0200

    fix app-routing-module login to / login#5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {HomeComponent} from "./home/home.component";
 //import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path:'', redirectTo:'login',pathMatch:'full'},
  /* {path: 'addCustomer', component: CustomerAddComponent },
   {path: 'login', component: LoginComponent },
   {path: 'home', component: HomeComponent },*/
