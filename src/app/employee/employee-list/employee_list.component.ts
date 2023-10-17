2ef1405fbee042cfa026edee267829fddd7ced35
Kara <blk20100@gmail.com>
2023-10-17 23:21:36 +0200

    Added tolist employees sort by name,lastname and city and navigation pages gestionEmpleados#3

--- src/app/employee/employee-list/employee_list.component.ts
+++ src/app/employee/employee-list/employee_list.component.ts
@@ -13,8 +13,11 @@ export class Employee_listComponent {
   employees: Array<EmployeeI>;
   page = 0;
   size = 10;
-  order = "lastName1";
+  order = "employeeName";
   acs = true;
+  isFirstPage = false;
+  isLastPage = false;
+  totalPages: Array<number>;
 
   constructor(private employeeService: EmployeeService){  }
 
@@ -24,10 +27,39 @@ export class Employee_listComponent {
 
   getEmployeeList(){
     this.employeeService.getEmployeeListPage(this.page,this.size,this.order,this.acs).subscribe(data => {
-      debugger;
       // @ts-ignore
       this.employees = data.content;
+      // @ts-ignore
+      this.isFirstPage = data.first;
+      // @ts-ignore
+      this.isLastPage = data.last;
+      // @ts-ignore
+      this.totalPages = new Array(data['totalPages']);
       }
     )
+  }
+
+  sort(order : string) {
+    this.acs = !this.acs;
+    this.order = order;
+    this.getEmployeeList();
+  }
+
+  rewind() {
+    if (!this.isFirstPage){
+      this.page--;
+      this.getEmployeeList();
+    }
+  }
+  forward() {
+    if (!this.isLastPage){
+      this.page++;
+      this.getEmployeeList();
+    }
+  }
+
+  setPage(page: number){
+    this.page = page;
+    this.getEmployeeList();
   }
 }
